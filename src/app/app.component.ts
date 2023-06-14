2bef5798935bde99b7ed44e5b22eb86d5d28b8e7
Esteban Jimenez <estebandjs7@gmail.com>
2023-06-14 11:02:44 -0400

    reportes de mascotas v1 angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+ 
+  reportes = [
+    {
+      img: "https://images.ecestaticos.com/K-H13opp5EdmIWo61v0sXMfywco=/0x87:2155x1283/557x418/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F8ec%2F08c%2F85c%2F8ec08c85c866ccb70c4f1c36492d890f.jpg",
+      titulo: 'Perro Perdido',
+      descripcion: 'Descripción del reporte 1',
+      fechaUltimaVezVisto: '2023-06-10',
+      telefono: '123456789',
+      pais: 'Paraguay',
+      linkCompleto: 'https://www.example.com/reporte1',
+      visible: true
+    },
+    {
+      img: "https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.webp?w=1600&h=1067",
+      titulo: 'Gato Perdido',
+      descripcion: 'Descripción del reporte 2',
+      fechaUltimaVezVisto: '2023-06-12',
+      telefono: '987654321',
+      pais: 'Paraguay',
+      linkCompleto: 'https://www.example.com/reporte2',
+      visible: false
+    },
+  ];
 }
